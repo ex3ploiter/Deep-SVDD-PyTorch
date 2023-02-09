463f8ddb84a79adc4eb58e51fdd47aad1ce29e77
ex3ploiter <52802071+ex3ploiter@users.noreply.github.com>
2023-02-09 16:36:10 +0330

    arg

--- src/main.py
+++ src/main.py
@@ -53,6 +53,10 @@ from datasets.main import load_dataset
               help='Number of workers for data loading. 0 means that the data will be loaded in the main process.')
 @click.option('--normal_class', type=int, default=0,
               help='Specify the normal class of the dataset (all other classes are considered anomalous).')
+@click.option('--eps', type=float, default=8/255)            
+@click.option('--alpha', type=float, default=1e-2)            
+
+
 def main(dataset_name, net_name, xp_path, data_path, load_config, load_model, objective, nu, device, seed,
          optimizer_name, lr, n_epochs, lr_milestone, batch_size, weight_decay, pretrain, ae_optimizer_name, ae_lr,
          ae_n_epochs, ae_lr_milestone, ae_batch_size, ae_weight_decay, n_jobs_dataloader, normal_class):
