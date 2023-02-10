f8b6835b844708045c0af556426525c0d6ea9084
ex3ploiter <52802071+ex3ploiter@users.noreply.github.com>
2023-02-11 03:01:38 +0330

    bug

--- src/datasets/main.py
+++ src/datasets/main.py
@@ -7,7 +7,7 @@ from .mvtec import MVTec_Dataset
 def load_dataset(dataset_name, data_path, normal_class):
     """Loads the dataset."""
 
-    implemented_datasets = ('mnist', 'cifar10','fashionmnist','svhn')
+    implemented_datasets = ('mnist', 'cifar10','fashionmnist','svhn','mvtec')
     assert dataset_name in implemented_datasets
 
     dataset = None
